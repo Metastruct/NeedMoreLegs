8f4db406b6c0426a3bf6a5b622514d2671840632
Python1320 <Python1320@users.noreply.github.com>
2015-07-22 13:14:14 +0300

    non admin only

--- lua/autorun/nml.lua
+++ lua/autorun/nml.lua
@@ -43,7 +43,7 @@ for mechname,data in next, mechs or {} do
 										mechtype = mechname
 									},
 			Author                  = "NML",
-			AdminOnly               = true,
+			AdminOnly               = false,
 			Information             = "A NeedMoreLegs mech"
 	} )
 
